ab7997a80be85a29545d335e13a44ed2ec40d8dc
Chris Tan <csytan@gmail.com>
2014-04-25 12:42:40 -0400

    First commit

---
+++ static/script.js
@@ -0,0 +1,101 @@
+$(function(){
+
+$('#map')
+    .on('dragenter', function(e){
+        $(this).css('border', '2px solid #0B85A1');
+        return false;
+    })
+    .on('dragover', function(e){
+        return false;
+    })
+    .on('drop', function(e){
+        $(this).css('border', 'none');
+        var files = e.originalEvent.dataTransfer.files;
+        var reader = new FileReader();
+        reader.onload = function(e){
+            csv = reader.result;
+            doStuff(csv);
+        };
+        reader.readAsText(files[0]);
+        return false;
+    });
+
+
+//doStuff(TEST_CSV);
+
+
+function doStuff(csv){
+    var nodes = nodesFromCSV(csv);
+    var nodeMap = {};
+    _.each(nodes, function(node){
+        nodeMap[node.id] = node;
+    });
+    var map = L.map('map').setView([nodes[0].lat, nodes[0].lon], 8);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    for (var i=0, node; node=nodes[i]; i++){
+        //L.marker([node.lat, node.long]).addTo(map);
+    }
+
+    L.geoJson(GEOJSON).addTo(map);
+
+    var worker = new Worker('/static/worker.js');
+
+    worker.addEventListener('message', function(e){
+        var msg = e.data;
+        if (msg.cmd == 'status'){
+            $('.status').text(msg.status);
+        } else if (msg.cmd == 'add_edge'){
+            var edge = msg.edge;
+            var node_a = nodeMap[edge.a];
+            var node_b = nodeMap[edge.b];
+            L.polyline([
+                L.latLng(node_a.lat, node_a.lon),
+                L.latLng(node_b.lat, node_b.lon)], {color: 'red'}).addTo(map);
+        }
+    }, false);
+
+    worker.postMessage(nodes);
+}
+
+
+
+function nodesFromCSV(str){
+    var csv = str.split(/[\r\n|\n]+/);
+    var wgs84 = "+proj=longlat +zone=29 +ellps=WGS84 +datum=WGS84 +no_defs"
+    var proj = csv[0].indexOf('PROJ.4 ') === 0 ? csv.shift().replace('PROJ.4 ', '') : null;
+    proj = proj ? proj4(proj, wgs84) : null;
+    var headers = csv.shift().toLowerCase().split(',');
+
+    var nodes = [];
+    for (var i=0, line; line=csv[i]; i++){
+        var values = line.split(',');
+        var node = {};
+        for (var j=0, value; value=values[j]; j++){
+            node[headers[j]] = value;
+        }
+        nodes.push(node);
+    }
+
+    // Projection conversions
+    for (var i=0, node; node=nodes[i]; i++){
+        if (typeof node.id == 'undefined'){
+            node.id = i;
+        }
+        if (proj){
+            var latLon = proj.forward([parseInt(node.x), parseInt(node.y)]);
+            node.lat = latLon[0];
+            node.lon = latLon[1];
+        } else {
+            node.lat = parseFloat(node.lat);
+            node.lon = parseFloat(node.lon);
+        }
+    }
+    return nodes;
+}
+
+
+});
---
+++ static/worker.js
@@ -0,0 +1,100 @@
+self.importScripts('/static/worker-libs.js');
+
+
+self.addEventListener('message', function(e){
+    var nodes = e.data;
+    Network.minimumSpanningTree(nodes);
+}, false);
+
+
+
+var Network = {};
+Network.lineToPoints = function(lat1, lon1, lat2, lon2, interval){
+    // Splits a line into multiple points
+    // interval: distance in km
+    var bearing = this.getBearing(lat1, lon1, lat2, lon2);
+    var distance = this.distanceFromPoint(lat1, lon1, lat2, lon2);
+    var points = [];
+    for (var i=0, d; d=i*interval && d<=distance;i++){
+        var point = this.pointFromBearing(lat1, lon1, bearing, d);
+        points.push(point);
+    }
+    points.push([lat2, lon2]);
+    return points;
+};
+Network.getBearing = function(lat1, lon1, lat2, lon2){
+    // http://www.movable-type.co.uk/scripts/latlong.html
+    var dLon = (lon2 - lon1).toRad();
+    var y = Math.sin(dLon) * Math.cos(lat2);
+    var x = Math.cos(lat1) * Math.sin(lat2) -
+            Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
+    return Math.atan2(y, x).toDeg();
+};
+Network.pointFromBearing = function(lat, lon, bearing, distance){
+    // http://www.movable-type.co.uk/scripts/latlong.html
+    var R = 6371;
+    var lat2 = Math.asin(
+        Math.sin(lat) * Math.cos(distance/R) + 
+        Math.cos(lat) * Math.sin(distance/R) * Math.cos(brng));
+    var lon2 = lon + Math.atan2(
+        Math.sin(bearing) * Math.sin(distance/R) * Math.cos(lat1), 
+        Math.cos(d/R) - Math.sin(lat1) * Math.sin(lat2));
+    return [lat2, lon2];
+};
+Network.distanceFromPoint = function(lat1, lon1, lat2, lon2){
+    // http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
+    var R = 6371; // Radius of the earth in km    
+    var dLat = this.degToRad(lat2 - lat1);
+    var dLon = this.degToRad(lon2 - lon1); 
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(this.degToRad(lat1)) * Math.cos(this.degToRad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    return R * c; // Distance in km
+};
+Network.degToRad = function(deg){
+    return deg * Math.PI / 180;
+};
+Network.minimumSpanningTree = function(nodes){
+    // Kruskal's Algorithm
+    // http://architects.dzone.com/articles/algorithm-week-kruskals
+    var edges = [];
+    for (var i=0, node_a; node_a=nodes[i]; i++){
+        for (var j=i+1, node_b; node_b=nodes[j]; j++){
+            edges.push({
+                a: node_a.id,
+                b: node_b.id,
+                weight: this.distanceFromPoint(node_a.lat, node_a.lon, node_b.lat, node_b.lon)
+            });
+        }
+    }
+
+    edges.sort(function(a, b){
+        return a.weight - b.weight;
+    });
+
+    var forest = {};
+    _.each(nodes, function(node){
+        forest[node.id] = node.id;
+    });
+
+    _.each(edges, function(edge){
+        // If both edge vertices are in different sets, combine them
+        if (forest[edge.a] !== forest[edge.b]){
+            var a_value = forest[edge.a];
+            var b_value = forest[edge.b];
+            _.each(forest, function(value, id){
+                if (value === b_value){
+                    forest[id] = a_value;
+                }
+            });
+            // Add edge to tree
+            self.postMessage({
+                cmd: 'add_edge',
+                edge: edge
+            });
+        }
+    });
+};
+
+
